778be8dd07a47c4fba34b5f0b038bb9577af75ff
Mike <michaellarrubis@gmail.com>
2020-01-26 20:35:58 +0800

    Fix error on precompile

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -16,7 +16,7 @@ document.addEventListener("turbolinks:load", function() {
   tagItems = document.querySelectorAll('.js-color');
   colorArray = ["#ef5350", "#ec407a", "#ab47bc", "#5c6bc0", "#42a5f5", "#26a69a", "#ffa726", "#ff7043", "#8d6e63"]
 
-  for( let i = 0; i < tagItems.length; i++ ) {
+  for( var i = 0; i < tagItems.length; i++ ) {
     tagItems[i].style.backgroundColor = colorArray[Math.floor(Math.random() * colorArray.length)];
     tagItems[i].style.opacity = 0.5;
   }
